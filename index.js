0b47294f8d96a5de47afd506585f6b0db212c664
umarprogrammer19 <umarprogrammer19666@gmail.com>
2025-01-25 15:40:55 +0500

    Authentication Complete

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
-import express from "express";
+import express, { urlencoded } from "express";
 import "dotenv/config";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 import connectdb from "./src/database/index.js";
+import authRouter from "./src/routes/auth.routes.js"
 
 const app = express();
 const port = process.env.PORT;
 
+const corsOption = {
+    origin: "http://localhost:3000",
+    credentials: true,
+};
+app.use(cors(corsOption));
+app.use((urlencoded({ extended: false })));
 app.use(express.json());
+app.use(cookieParser());
+app.use('/api/v1', authRouter);
 
 app.get("/", (req, res) => {
-    res.send("Hello World");
+    res.send("SMIT GRAND FINALS");
 });
+
 
 connectdb()
     .then(() => {
