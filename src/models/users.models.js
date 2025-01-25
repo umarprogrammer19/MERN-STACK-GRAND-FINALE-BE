b20e60cf791aba0d1792782cb0247e5eae8450d7
umarprogrammer19 <umarprogrammer19666@gmail.com>
2025-01-25 15:22:23 +0500

    User Model

---
+++ src/models/users.models.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt"
+
+const userSchema = new mongoose.Schema({
+    fullname: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    imageURL: {
+        type: String,
+        require: true,
+    }
+}, { timestamps: true })
+
+userSchema.pre("save", async function (next) {
+    const user = this;
+    if (!user.isModified("password")) return;
+    const hashPassword = await bcrypt.hash(user.password, 10);
+    user.password = hashPassword;
+    next();
+})
+
+export default mongoose.model('users', userSchema);
